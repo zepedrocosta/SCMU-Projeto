c3dc2fe5f19880d4852306659b49e3606f0fb150
RafaelCMira <113689920+RafaelCMira@users.noreply.github.com>
2025-05-25 00:16:09 +0100

    Improved layout, wrapped app layout to take phone display into account

--- mobile-app/app/app/_layout.tsx
+++ mobile-app/app/app/_layout.tsx
@@ -1,7 +1,9 @@
+// filepath: c:\Users\Rafael\Documents\Universidade\2024_2025\2 Semestre\SCMU\projeto\repo\SCMU-Projeto\mobile-app\app\app\_layout.tsx
 import { Slot } from "expo-router";
 import { PaperProvider } from "react-native-paper";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StateProvider } from "../context/StateContext";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 const queryClient = new QueryClient();
 
@@ -10,7 +12,14 @@ export default function Layout() {
 		<QueryClientProvider client={queryClient}>
 			<StateProvider>
 				<PaperProvider>
-					<Slot />
+					<SafeAreaProvider>
+						<SafeAreaView
+							style={{ flex: 1 }}
+							edges={["top", "bottom", "left", "right"]}
+						>
+							<Slot />
+						</SafeAreaView>
+					</SafeAreaProvider>
 				</PaperProvider>
 			</StateProvider>
 		</QueryClientProvider>
